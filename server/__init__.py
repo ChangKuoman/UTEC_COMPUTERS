d073ce450c062fba4b4173134e2a13991d54caef
Susana Chang <samanta.chang@utec.edu.pe>
2022-06-06 22:58:03 -0500

    Organizando archivos

--- server/__init__.py
+++ server/__init__.py
@@ -4,7 +4,6 @@ from flask import (
     jsonify,
     request
 )
-from flask_cors import CORS
 from models.MotherBoard import MotherBoard
 from models.Component import Component
 from models.User import User
@@ -16,13 +15,6 @@ from config import setup_db
 def create_app(test_config=None):
     app = Flask(__name__)
     setup_db(app)
-    CORS(app)
-
-    @app.after_request
-    def after_requests(response):
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorizations, true')
-        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
-        return response
 
     @app.route('/', methods=['GET'])
     def index():
