fa0c78735595becc6d3b9987aea30a49d18139d5
Susana Chang <samanta.chang@utec.edu.pe>
2022-06-08 11:07:28 -0500

    default en el modelo

--- models/SimulationComponent.py
+++ models/SimulationComponent.py
@@ -3,7 +3,7 @@ from sqlalchemy import func, ForeignKey
 
 
 class SimulationComponent(db.Model):
-    __tablename__ = 'simulation_components'
+    __tablename__ = 'simulation_component'
     id_simulation = db.Column(db.Integer, ForeignKey('simulation.id'), primary_key=True)
     id_component = db.Column(db.Integer, ForeignKey('component.id'), primary_key=True)
 
