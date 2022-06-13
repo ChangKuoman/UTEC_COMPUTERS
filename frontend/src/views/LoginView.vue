c693c1a7a647db7d417508ca53de60ef287efe47
Susana Chang <samanta.chang@utec.edu.pe>
2022-06-12 21:49:11 -0500

    login check input form

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -2,21 +2,18 @@
     <div>
         <div class="contenedor_HOME">
             <div class="Texto_presentacion">
-                <form>
+                <form @change = "error.clear">
                     <p>USERNAME</p>
                     <input v-model = "username" type="text"/>
                     <p>PASSWORD</p>
                     <input v-model = "password" type="password"/>
-                    <button @click.prevent = "login">LOGIN</button>
+                    <button @click.prevent = "error.clear(); check_form_login()">LOGIN</button>
                 </form>
             </div>
-            
         </div>
-
-
-        <div v-if = "wentWrong">
-            Username is not valid or password is wrong
-        </div>
+        <ul class = "no-dots" v-if = "error">
+            <li v-for = "(error, index) in error.list" :key = "index">{{error}}</li>
+        </ul>
         <FooterComponent/>
     </div>
 </template>
@@ -28,12 +25,33 @@ export default {
   props: [],
   data () {
     return {
-        wentWrong: false,
+        error: {
+            show: false,
+            list: [],
+            clear: () => {
+                this.error.show = false
+                this.error.list = []
+            }
+        },
         username: '',
         password: ''
     }
   },
   methods: {
+    check_form_login () {
+        if (this.username === '') {
+            this.error.list.push('Username cannot be empty')
+        }
+        if (this.password === '') {
+            this.error.list.push('Password cannot be empty')
+        }
+        if (this.error.list.length) {
+            this.error.show = true
+        }
+        if (this.error.show === false) {
+            this.login()
+        }
+    },
     login () {
         fetch('http://127.0.0.1:5000/users', {
             method: 'POST',
@@ -69,11 +87,13 @@ export default {
                 this.$router.push('/simulator')
             }
             else {
-                console.log(JsonResponse)
+                this.error.list.push('Username is not valid or password is wrong')
+                this.error.show = true
             }
         })
         .catch(() => {
-            console.log('error')
+            this.error.list.push('Something went wrong!')
+            this.error.show = true
         })
     }
   }
