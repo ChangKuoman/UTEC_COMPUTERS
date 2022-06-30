81e2e12bb850e893f0d3865d08cc3b82de82a693
Susana Chang <samanta.chang@utec.edu.pe>
2022-06-30 17:11:45 -0500

    mensaje arreglado

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,13 +4,13 @@
             <div class="contenedor_HOME">
                 <div class="Texto_presentacion">
                     <p>
-                        Our project is a web application made with the Flask micro Framework
-                        that allows the simulated purchase of a computer taking into account
-                        the motherboard and its compatibility with the different components
-                        as the base piece. The information in the database is based on different
-                        web pages for the purchase of specialized technology in computers
-                        such as LoginStore, and proprietary brands such as MSI, AMD, Intel,
-                        among others.
+                        Our project is a web application that allows the simulated purchase
+                        of a computer taking into account the motherboard as the base piece
+                        and its compatibility with the different components. The information
+                        in the database is based on different web pages for the purchase of
+                        specialized technology in computers such as LoginStore, and proprietary
+                        brands such as MSI, AMD, Intel, among others. It is made with the Flask
+                        micro Framework for the backend and the Vue Framework for the frontend.
                     </p>
                     <img class="img-150" src="@/assets/img/cat.gif" />
                     <a href="https://github.com/ChangKuoman/UTEC_COMPUTERS" target="_target">⇨ LINK TO GITHUB </a>
