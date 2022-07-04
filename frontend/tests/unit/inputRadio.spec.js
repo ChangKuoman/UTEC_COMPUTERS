de21626c0fcaafec0209ea522b0301009b9fd270
Susana Chang <samanta.chang@utec.edu.pe>
2022-07-04 18:30:20 -0500

    unittest inputS y home

--- frontend/tests/unit/inputRadio.spec.js
+++ frontend/tests/unit/inputRadio.spec.js
@@ -1,12 +1,39 @@
 import { mount } from "@vue/test-utils";
 import InputRadio from "@/components/InputRadio.vue";
 
-test("input-radio", async () => {
+test("input-radio-1", async () => {
   const wrapper = mount(InputRadio, {
     props: {
       title: "Titulo",
     },
   });
 
-  expect(wrapper.find("h2").text()).toBe("Titulo");
+  expect(wrapper.find("h4").text()).toBe("Titulo");
+});
+
+test("input-radio-2", async () => {
+  const wrapper = mount(InputRadio, {
+    props: {
+      title: "Titulo",
+      objects: [
+        {
+          name: "product1",
+          description: "description1",
+          price: 15.8
+        },
+        {
+          name: "product2",
+          description: "description2",
+          price: 48.5
+        }
+      ],
+      modelValue: {
+        name: "product1",
+        description: "description1",
+        price: 15.8
+      }
+    },
+  });
+
+  expect(wrapper.findAll("li")).toHaveLength(2);
 });
