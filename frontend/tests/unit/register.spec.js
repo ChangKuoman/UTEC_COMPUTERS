fa7c5f39b78b18e547ca5d2723c6ea7bde7fb357
Susana Chang <samanta.chang@utec.edu.pe>
2022-07-04 18:24:56 -0500

    unittest register

--- frontend/tests/unit/register.spec.js
+++ frontend/tests/unit/register.spec.js
@@ -1,8 +1,26 @@
 import { mount } from "@vue/test-utils";
 import RegisterView from "@/views/RegisterView.vue";
 
-test("register", async () => {
+test("register-1", async () => {
   const wrapper = mount(RegisterView);
 
   expect(wrapper.find("button").text()).toBe("REGISTER");
+});
+
+test("register-2", async () => {
+  const wrapper = mount(RegisterView);
+
+  expect(wrapper.findAll("form")).toHaveLength(1);
+});
+
+test("register-3", async () => {
+  const wrapper = mount(RegisterView);
+
+  expect(wrapper.find("button").classes()).toContain("buttom1");
+});
+
+test("register-4", async () => {
+  const wrapper = mount(RegisterView);
+
+  expect(wrapper.findAll("div")[1].classes()).toContain("contenedor_HOME");
 });
