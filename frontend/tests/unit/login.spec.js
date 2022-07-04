f44b4db5f16ad25115156006d0fd0846c0af2977
Susana Chang <samanta.chang@utec.edu.pe>
2022-07-04 18:28:15 -0500

    unittest passworg y login

--- frontend/tests/unit/login.spec.js
+++ frontend/tests/unit/login.spec.js
@@ -1,8 +1,26 @@
 import { mount } from "@vue/test-utils";
 import LoginView from "@/views/LoginView.vue";
 
-test("login", async () => {
+test("login-1", async () => {
   const wrapper = mount(LoginView);
 
   expect(wrapper.find("button").text()).toBe("LOGIN");
+});
+
+test("login-2", async () => {
+  const wrapper = mount(LoginView);
+
+  expect(wrapper.find("button").classes()).toContain("buttom1");
+});
+
+test("login-3", async () => {
+  const wrapper = mount(LoginView);
+
+  expect(wrapper.findAll("div")[1].classes()).toContain("contenedor_HOME");
+});
+
+test("login-4", async () => {
+  const wrapper = mount(LoginView);
+
+  expect(wrapper.findAll("div")[2].classes()).toContain("Texto_presentacion");
 });
